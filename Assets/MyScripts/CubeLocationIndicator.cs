1bc82000d8eb0242c4bf01880fe5c0b2c33fc327
CJStoops <1021467@student.sae.edu.au>
2021-04-19 00:48:53 +1000

    Week_11
    Migrated scripts across from Mechanical Chess.  Applied sounds to their appropriate locations. Chose to make individual, transparent chess squares that light up when interacted with.  Completed placing said squares across the chess board.  Assigned chess movement sound to each square.

---
+++ Assets/MyScripts/CubeLocationIndicator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceLocationIndicator : MonoBehaviour
+{
+    public Renderer rPawn, rKing;
+
+    public Material red, transparent;
+    // Start is called before the first frame update
+
+    AudioSource myAudioSource;
+    public AudioClip soundFX;
+    void Start()
+    {
+        // r = GetComponent<Renderer>();
+        myAudioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Pawn")
+        {
+            rPawn.material = red;
+            myAudioSource.Play();
+
+            OVRHapticsClip ovrClip = new OVRHapticsClip(soundFX);
+            OVRHaptics.RightChannel.Preempt(ovrClip);
+            OVRHaptics.LeftChannel.Preempt(ovrClip);
+
+        }
+        else if (other.tag == "King")
+        {
+            rKing.material = red;
+            myAudioSource.Play();
+
+            OVRHapticsClip ovrClip = new OVRHapticsClip(soundFX);
+            OVRHaptics.RightChannel.Preempt(ovrClip);
+            OVRHaptics.LeftChannel.Preempt(ovrClip);
+        }
+        else
+        {
+            //not king or pawn, so do nothing.
+        }
+
+
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        //r.material.color = Color.grey;
+        rPawn.material = transparent;
+        rKing.material = transparent;
+    }
+
+
+}
+
---
+++ Assets/MyScripts/PieceLocationIndicator.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeLocationIndicator : MonoBehaviour
+{
+    public Renderer rCube;
+    bool piecePlaced = false;
+
+    public Material red, transparent;
+    // Start is called before the first frame update
+
+    AudioSource myAudioSource;
+    public AudioClip soundFX;
+    void Start()
+    {
+        // r = GetComponent<Renderer>();
+        myAudioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "ChessPiece" && piecePlaced == false)
+        {
+            rCube.material = red;
+            myAudioSource.Play();
+
+            OVRHapticsClip ovrClip = new OVRHapticsClip(soundFX);
+            OVRHaptics.RightChannel.Preempt(ovrClip);
+            OVRHaptics.LeftChannel.Preempt(ovrClip);
+            piecePlaced = true;
+            Invoke("SetTransparent", 2f);
+        }
+            else
+        {
+            //not chess piece, so do nothing.
+        }           
+    
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        //r.material.color = Color.grey;
+       
+        piecePlaced = false;
+     
+    }
+
+    private void SetTransparent()
+    {
+        rCube.material = transparent;
+    }
+
+}
+
